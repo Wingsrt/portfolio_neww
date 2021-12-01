069b18f5461f0cb6609aae0274f69f7d4bd94a01
Wingsrt <squirrelstudio0@gmail.com>
2021-12-01 22:50:54 +0530

    first commit

---
+++ src/data/aboutData.js
@@ -0,0 +1,7 @@
+export const aboutData = {
+    title: "Who I am",
+    // description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
+    description1:"I have worked on several different projects collaboratively and solely. I am a self-driven and passionate web developer skilled in JavaScript, React, and Redux and familiar with front-end frameworks like MaterialUI. Gone through 30 weeks of coding Bootcamp at Masai School.Also, worked on NodeJs, Express, MongoDB DataBase, and also familiar with Redis. Always eager to learn new technologies to enhance my skills and bring value to the organization",
+    // description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    image: 2
+}
---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'pathakshantanu94@gmail.com',
+    phone: '+917447556344',
+    address: 'PLOT 5 ,GODHANI ROAD MANKAPUR , NAGPUR ,MAHARASHTRA',
+
+    sheetAPI: ''
+}
